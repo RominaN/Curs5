5ec219afbf9d15cb338d64513f62abf69164d0ea
Romina <44588411+RominaN@users.noreply.github.com>
2020-03-03 22:10:55 +0200

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs5._1
+{
+    class Animal
+    {
+        public string nume;
+        public int varsta;
+
+
+        public void CaracteristiciAnimal()
+        {
+            Console.WriteLine("Name:" + nume);
+            Console.WriteLine("Age:" + varsta);
+        }
+
+    }
+
+    class Caine : Animal
+    {
+        public void Latra()
+        {
+            Console.WriteLine("Ham");
+        }
+    }
+    class Pisica : Animal
+    {
+
+        public void Miauna()
+        {
+            Console.WriteLine("Miau");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caine catel = new Caine();
+            catel.nume = "Azorel";
+            catel.varsta = 2;
+            catel.CaracteristiciAnimal();
+            catel.Latra();
+
+            Pisica miaunel = new Pisica();
+            miaunel.nume = "Blanosul";
+            miaunel.varsta = 1;
+            miaunel.CaracteristiciAnimal();
+            miaunel.Miauna();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
